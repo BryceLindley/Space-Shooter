1b9a00ece760e75d676a7f8a4dbfd1941c3b2294
BryceLindley <lindleybryce@gmail.com>
2021-08-24 09:13:35 -0700

    EnemySpawner gameobject created.

--- Assets/Assets/Resources/Script/EnemySpawner.cs
+++ Assets/Assets/Resources/Script/EnemySpawner.cs
@@ -9,19 +9,19 @@ public class EnemySpawner : MonoBehaviour {
 	[SerializeField] [Range(0, 10)] int quantity;
 	GameObject enemies;
 
-  
+
 	// Use this for initialization
-	  void Start () {
-		
+	void Start() {
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+
 	}
 
 	void Awake()
-    {
+	{
 		// make instance of empty game object
 		enemies = GameObject.Find("_Enemies");
 		StartCoroutine(FireEnemy(quantity, spawnRate));
@@ -31,15 +31,23 @@ public class EnemySpawner : MonoBehaviour {
 	{
 		for (int i = 0; i < qty; i++)
 		{
-		GameObject enemyUnit = CreateEnemy();
-		enemyUnit.gameObject.transform.SetParent(this.transform);
-		enemyUnit.transform.position = transform.position;
-		yield return new WaitForSeconds(spwnRte);
+			GameObject enemyUnit = CreateEnemy();
+			enemyUnit.gameObject.transform.SetParent(this.transform);
+			enemyUnit.transform.position = transform.position;
+			yield return new WaitForSeconds(spwnRte);
+		}
+		yield return null;
 	}
-	yield return null;
-  }
 
-
-	
-
+	GameObject CreateEnemy()
+	{
+		GameObject enemy = GameObject.Instantiate(actorModel.actor) as GameObject;
+		enemy.GetComponent<InterfaceActorTemplate>().ActorStats(actorModel);
+		enemy.name = actorModel.actorName.ToString();
+		return enemy;
+	}
 }
+		
+
+
+

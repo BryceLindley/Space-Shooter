e7ffa1b2869cccbeb8ab7e458186284c98100e09
BryceLindley <lindleybryce@gmail.com>
2021-09-20 14:51:48 -0700

    PlayerShipBuild Script

---
+++ Assets/Assets/Resources/Script/PlayerShipBuild.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipBuild : MonoBehaviour {
+
+	[SerializeField]
+	GameObject[] shopButtons;
+	GameObject target;
+	GameObject tmpSelection;
+
+	// Use this for initialization
+	void Start () {
+		TurnOffSelectionHighlights();
+	}
+
+	void Update()
+	{
+		AttemptSelection();
+	}
+
+	// Within the TurnOffSelectionHighlights method, we run a for loop that makes sure all of the buttons have their blue rectangles turned off.
+	  void TurnOffSelectionHighlights()
+    {
+	for (int i = 0; i < shopButtons.Length; i++)
+        {
+			shopButtons[i].SetActive(false);
+       }
+    }
+
+	//Within this method, we reset the target game object to remove any previous data.
+	//We then take reference from the camera to find where the player tapped or clicked
+	//their mouse on the screen and store the result in the form of a ray. 
+	GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+		GameObject target = null;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray.origin, ray.direction * 100, out hit))
+        {
+			target = hit.collider.gameObject;
+        }
+		return target;
+    }
+
+	// The AttemptSelection method will check when a condition is made when the player
+	// has made contact by tapping the screen or clicking a mouse button in our shop scene.
+	  void AttemptSelection()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo;
+			target = ReturnClickedObject(out hitInfo);
+			if (target != null)
+			{
+				if (target.transform.Find("itemText"))
+				{
+					TurnOffSelectionHighlights();
+					Select();
+				}
+			}
+		}
+	}
+
+	//The Select method doesn't need to check any conditions with if statements as this
+	//has mostly been done for us with the previous code.
+	//We carry out a search for SelectionQuad and store its reference as tmpSelection.
+	//Finally, we set the tmpSelection game objects activity to true so that it is seen in our shop Scene window.
+
+  void Select()
+	{
+		tmpSelection = target.transform.Find
+		   ("SelectionQuad").gameObject; tmpSelection.SetActive(true);
+	}
+
+}
---
+++ Assets/Assets/Resources/Script/ShopPiece.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopPiece : MonoBehaviour
+{
+	// Drag and drop each scriptable asset file to its field in the Unity editor
+	// refer to the ShopSelection property that will receive and sent its data (get and set)
+	[SerializeField]
+	SOShopSelection shopSelection;
+
+	void Awake()
+	{
+		// icon slot
+		//Inside the function are two if statements; the first checks whether there is a SpriteRenderer component in any of its child game objects.
+		//If there is, then it grabs reference from its shopSelection asset file and applies its icon sprite to display on the button.
+		if (GetComponent<SpriteRenderer>() != null)
+		{
+			GetComponentInChildren<SpriteRenderer>().sprite = shopSelection.icon;
+		}
+		//selection value
+		// The alternative if statement checks whether any of the child game objects of this class have a game object titled itemText. 
+		// If there is a game object titled itemText, we update its TextMesh component's text value with the shopSelection cost value.
+		if (transform.Find("itemText"))
+		{
+			GetComponentInChildren<TextMesh>().text = shopSelection.cost;
+		}
+
+	}
+	public SOShopSelection ShopSelection
+	{
+		get { return shopSelection; }
+		set { shopSelection = value; }
+	}
+}
+
+	
+
+
+
+

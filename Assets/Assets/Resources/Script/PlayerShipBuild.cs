73b905bafa6855dfd4e42f9d59e43bdf2e094027
BryceLindley <lindleybryce@gmail.com>
2021-09-21 19:57:25 -0700

    UpgradeToShip method

--- Assets/Assets/Resources/Script/PlayerShipBuild.cs
+++ Assets/Assets/Resources/Script/PlayerShipBuild.cs
@@ -89,9 +89,14 @@ public class PlayerShipBuild : MonoBehaviour {
 						SoldOut();
                     }
 				}
+				else if(target.name == "BUY?")
+                {
+					BuyItem();
+                }
 			}
 		}
 	}
+
 
 	//The Select method doesn't need to check any conditions with if statements as this
 	//has mostly been done for us with the previous code.
@@ -113,6 +118,88 @@ public class PlayerShipBuild : MonoBehaviour {
 		textBoxPanel.transform.Find("desc").gameObject.GetComponent<TextMesh>().text = tmpSelection.GetComponentInParent<ShopPiece>().ShopSelection.description;
 	}
 
+	//The Affordable method checks whether the bank integer(which currently contains the value 600) is equal or greater than the
+	//value of the button that we have selected(target). Next is an if statement that checks whether the bank integer value is
+	//greater than or equal to the string cost value of the selected item.Because we can't compare the value of a string
+	// variable to an int variable, we need to convert the string variable to an int variable.
+	// To do this, we use System.Int32.Parse() and enter the ShopeSelection.cost string value in the parse brackets.
+  void Affordable()
+    {
+		if (bank >= System.Int32.Parse(target.transform.GetComponent<ShopPiece>().ShopSelection.cost)) {
+			Debug.Log("CAN BUY");
+			buyButton.SetActive(true);
+        }
+    }
+
+	// The second method we wrote earlier was the LackOfCredits method, which checks in a similar way by casting
+	// the TextMesh component value if it's less than the bank integer value.
+	// If it is, we send a "CAN'T BUY" message to Unity's Console window:
+	void LackOfCredits()
+	{
+		if (bank < System.Int32.Parse(target.transform.Find
+		  ("itemText").GetComponent<TextMesh>().text))
+		{
+			Debug.Log("CAN'T BUY");
+		}
+	}
+
+	void SoldOut()
+    {
+		Debug.Log("SOLD OUT");
+    }
+
+	void TurnOffPlayerShipVisuals()
+    {
+		for (int i = 0; i < visualWeapons.Length; i++)
+        {
+			visualWeapons[i].gameObject.SetActive(false);
+        }
+    }
+
+	//The method creates(instantiates) a Player_Ship game object from the Resources folder. We then turn off(enabled = false)
+	//its own script attachment; otherwise, we would be able to move and shoot with it in the shop scene.
+	//We then move the Player_Ship object completely out of the Scene / Game window view. Finally,
+	//we assign it the default PlayerShip asset file that we dragged and dropped into the scriptable object field.
+	void PreparePlayerShipForUpgrade()
+    {
+		playerShip = GameObject.Instantiate(Resources.Load("Prefab/Player/Player_Ship")) as GameObject;
+		playerShip.GetComponent<Player>().enabled = false;
+		playerShip.transform.position = new Vector3(0, 10000, 0);
+		playerShip.GetComponent<InterfaceActorTemplate>().ActorStats(defaultPlayerShip);
+	}
+
+	//We then set purchaseMade to true. This Boolean value is used later when we leave the shop scene to
+	//start the game.If purchaseMade is true, a set of procedures follows.The next line turns off the buyButton
+	//function as we no longer need to display the results. Finally, we remove the selection from the grid at the
+	//bottom of the screen as a refresh.
+   void BuyItem()
+    {
+		Debug.Log("PURCHASED");
+		purchasedMade = true;
+		buyButton.SetActive(false);
+		tmpSelection.SetActive(false);
+
+		for(int i = 0; i < visualWeapons.Length; i++)
+        {
+			// check whether slection made in the selection grid matches so we can see it
+			if (visualWeapons[i].name == tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName)
+            {
+				visualWeapons[i].SetActive(true);
+            }
+        }
+		//send our upgrades to our player's ship, along with our bank credit
+		UpgradeToShip(tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName);
+		bank = bank - System.Int32.Parse(tmpSelection.transform.parent.GetComponent<ShopPiece>().ShopSelection.cost);
+		bankObj.transform.Find("bankText").GetComponent<TextMesh>().text = bank.ToString();
+		tmpSelection.transform.parent.transform.Find("itemText").GetComponent<TextMesh>().text = "SOLD";
+	}
+
+	//which loads the game object of that particular ship part and attaches it to a ship that is away from the screen.
+	  void UpgradeToShip(string upgrade)
+	{
+		GameObject shipItem = GameObject.Instantiate(Resources.Load("Prefab/Player/" + upgrade)) as GameObject;
+		shipItem.transform.SetParent(playerShip.transform); shipItem.transform.localPosition = Vector3.zero;
+	}
 
 
 }
